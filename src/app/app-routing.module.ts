b2d06adf20d248533947f722af96ba6456729a98
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2021-03-30 12:55:18 +0300

    add auth pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './views/auth/auth.component';
+import { RegisterComponent } from './views/auth/register/register.component';
+import { SignInComponent } from './views/auth/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const authRoutes = [
+  { path: '', component: SignInComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'register', component: RegisterComponent },
+];
+
+const routes: Routes = [
+  { path: '', component: AuthComponent, children: authRoutes },
+  { path: 'auth', component: AuthComponent, children: authRoutes },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { AuthComponent } from './views/auth/auth.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SignInComponent } from './views/auth/sign-in/sign-in.component';
+import { RegisterComponent } from './views/auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthComponent,
+    SignInComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
